93f243f32f18b0f8d5244a677a99c462d22c3d0b
ravenusmc <mcuddy77@gmail.com>
2019-05-24 19:57:13 -0400

    first commit

---
+++ server/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+#importing code that I wrote
+from data import *
+
+# configuration
+DEBUG = True
+
+# instantiate the app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app)
+
+# sanity check route
+@app.route('/colorChart', methods=['GET', 'POST'])
+def colorChart():
+    if request.method == 'POST':
+        data = Data()
+        post_data = request.get_json()
+        yearOne = int(post_data['yearOne'])
+        yearTwo = int(post_data['yearTwo'])
+        data_set_length = data.buildColorChart(yearOne, yearTwo)
+        return jsonify(data_set_length)
+
+
+if __name__ == '__main__':
+    app.run()
