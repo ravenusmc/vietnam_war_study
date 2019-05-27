3ba5d696547d7ae1eb0af14f1bee1bd53a40dc11
ravenusmc <mcuddy77@gmail.com>
2019-05-27 14:42:58 -0400

    finished header section

--- client/src/components/home/Header.vue
+++ client/src/components/home/Header.vue
@@ -2,7 +2,10 @@
   <div>
 
     <header>
-      <img src='@/assets/img/rice_fields.jpg'>
+      <div>
+        <h1 class='title_header'>Vietnam</h1>
+      </div>
+      <!-- <img src='@/assets/img/rice_fields.jpg'> -->
     </header>
 
   </div>
@@ -16,19 +19,31 @@ export default {
 
 <style>
 
-/* header {
+header {
   background-image: url('../../assets/img/rice_fields.jpg');
   width: 100%;
+  height: 500px;
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
-} */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
 
 img {
   width: 100%;
   background-position: center;
   background-size: cover;
   background-repeat: no-repeat;
+}
+
+.title_header {
+  font-size: 12rem;
+  font-weight: 600;
+  letter-spacing: -.3rem;
+  color: #fff;
 }
 
 </style>
