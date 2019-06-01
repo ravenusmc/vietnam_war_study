44ecb421f931568c7bb43ae3976b20b51ef0bceb
ravenusmc <mcuddy77@gmail.com>
2019-06-01 16:10:39 -0400

    started work on form for data page

--- client/src/components/deaths/Form.vue
+++ client/src/components/deaths/Form.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
-    <h1>Hi</h1>
+    <form @submit="submitYears">
+      <h5>Select the Date:</h5>
+      <label>Year One:</label>
+       <input type="number" v-model='yearOne' name="yearOne" placeholder="Enter First Year">
+       <label>Year Two:</label>
+       <input type="number" v-model="yearTwo" name="yearTwo" placeholder="Enter Second Year">
+       <div>
+         <button class="btn btn-primary font" type="submit">Submit</button>
+       </div>
+    </form>
   </div>
 </template>
 
 <script>
 export default {
   name: 'name',
+  data() {
+    return {
+      yearOne: 0,
+      yearTwo: 0,
+    };
+  },
+  methods: {
+    submitYears(evt) {
+      evt.preventDefault();
+      alert('Mike')
+    }
+  }
 }
 </script>
 
