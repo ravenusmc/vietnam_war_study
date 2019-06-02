484bc20e225080235a53749d268180c4a5a21113
ravenusmc <mcuddy77@gmail.com>
2019-06-02 14:55:14 -0400

    added v-chart

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,14 @@ import router from './router';
 import store from './store';
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
+import Chart from 'v-chart-plugin'
 
 Vue.config.productionTip = false;
 
 //Material design
 Vue.use(VueMaterial)
+//V-chart plugin
+Vue.use(Chart);
 
 //Vue bus
 export const eventBus = new Vue()
