c3f6d3b59104062db440d5efe2df1057ac2ddc1c
ravenusmc <mcuddy77@gmail.com>
2019-06-02 14:44:47 -0400

    worked more on the form

--- client/src/components/deaths/Form.vue
+++ client/src/components/deaths/Form.vue
@@ -1,15 +1,21 @@
 <template>
   <div>
-    <form @submit="submitYears">
-      <h5>Select the Date:</h5>
-      <label>Year One:</label>
-       <input type="number" v-model='yearOne' name="yearOne" placeholder="Enter First Year">
-       <label>Year Two:</label>
-       <input type="number" v-model="yearTwo" name="yearTwo" placeholder="Enter Second Year">
-       <div>
-         <button class="btn btn-primary font" type="submit">Submit</button>
-       </div>
-    </form>
+
+    <section>
+
+      <form @submit="submitYears">
+        <h5>Select the Date by Year:</h5>
+        <label>Year One:</label>
+         <input type="number" v-model='yearOne' name="yearOne" placeholder="Enter First Year">&nbsp;
+         <label>Year Two:</label>
+         <input type="number" v-model="yearTwo" name="yearTwo" placeholder="Enter Second Year">
+         <div class='button_div'>
+           <button class="btn btn-primary font" type="submit">Submit</button>
+         </div>
+      </form>
+
+    </section>
+
   </div>
 </template>
 
@@ -38,4 +44,22 @@ export default {
 </script>
 
 <style scoped>
+
+section {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+form {
+  border: 2px solid black;
+  padding: 15px;
+  box-shadow: 5px 8px #888888;
+  border-radius: 10px;
+}
+
+.button_div {
+  margin-top: 10px;
+}
 </style>
