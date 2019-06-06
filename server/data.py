4d6ce8a12c7f8c83f680da0ce6810f56b1165550
ravenusmc <mcuddy77@gmail.com>
2019-06-05 20:03:34 -0400

    data csv file added

--- server/data.py
+++ server/data.py
@@ -5,4 +5,12 @@ import numpy as np
 import pandas as pd
 
 class Data():
-    
+
+    def __init__(self):
+        self.data = pd.read_csv('./data/VietnamConflict.csv')
+
+    def basic_info(self):
+        print(self.data.head())
+        
+one = Data()
+one.basic_info()
