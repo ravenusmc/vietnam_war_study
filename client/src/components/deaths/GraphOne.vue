683d904f5292dd7f8051668f15e7cfd03b9f871b
ravenusmc <mcuddy77@gmail.com>
2019-06-30 19:43:57 -0400

    added code to start building first graph

--- client/src/components/deaths/GraphOne.vue
+++ client/src/components/deaths/GraphOne.vue
@@ -11,6 +11,44 @@ import { mapGetters } from 'vuex';
 
 export default {
   name: 'GraphOne',
+  computed: {
+    buildGraph() {
+      return this.vBarChartData = {
+        chartType: "vBarChart",
+        label: true,
+        fill: 'green',
+        selector: "vChart",
+        title: "Roman Victories",
+        subtitle: "By 75 Year Intervals",
+        width: 500,
+        height: 500,
+        metric: ["Roman Victories", 'Total Battles'],
+        dim: "Interval",
+        data: this.graphOneData,
+        grid: {
+          enabled: true,
+          gridTicks: 25,
+        },
+        overrides: {
+          palette: {
+            fill: ['#A91DEB', '#4fc08d'],
+            stroke: '#41B883'
+          },
+          x: {
+            ticks: 20
+          },
+          y: {
+            axisWidth: 40,
+          },
+        },
+        legends: {
+          enabled: true,
+          height: 5,
+          width: 50,
+        },
+      }
+    }
+  },
   methods: {
     ...mapActions([
       'fetchGraphOneData',
