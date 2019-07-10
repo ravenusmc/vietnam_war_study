7bd3abf7de2e04e92767a675d56554f520793197
ravenusmc <mcuddy77@gmail.com>
2019-07-10 11:05:52 -0400

    added descriptive paragraph

--- client/src/components/deaths/Talk.vue
+++ client/src/components/deaths/Talk.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
 
-    <div>
-      <h3>Thoughts</h3>
+    <div class='paragraphArea'>
+      <h3>Thoughts on War Deaths</h3>
+      <p>
+        What stands out for me the most on this graph is how well it matches a
+        bell curve. The height of that curve is 1968 which I always knew was
+        the height of the Vietnam War. This, of course, was when the U.S. had over
+        500,000 military personal within the country. Thus, since there were more
+        people in country there was a greater chance for a death to occur.
+      </p>
     </div>
 
   </div>
@@ -15,4 +22,10 @@ export default {
 </script>
 
 <style scoped>
+
+.paragraphArea {
+  margin-left: 3%;
+  margin-right: 3%;
+  margin-top: 50px;
+}
 </style>
